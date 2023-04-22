cb1b20d30414fb216d83d3f7b9553aaabf309905
João Victor <jvcs.mecatronica@gmail.com>
2023-04-22 01:41:22 -0300

    clean up creation flow

--- src/config/pageBuilder.js
+++ src/config/pageBuilder.js
@@ -3,40 +3,61 @@ const {
     PageSizes
 } = require('pdf-lib');
 
-function setMargins(pdfPage, config) {
-    const { marginTop, marginBottom, marginLeft, marginRight } = config;
-    const margins = [marginTop, marginRight, marginBottom, marginLeft];
-    pdfPage.setMargins(...margins.map((m) => parseInt(m)));
+async function drawSection(pdfPage, section, pageConfig) {
+    const {
+        config: {
+            marginTop: sectionMarginTop = marginTop,
+            marginBottom: sectionMarginBottom = marginBottom,
+            marginLeft: sectionMarginLeft = marginLeft,
+            marginRight: sectionMarginRight = marginRight
+        },
+        content
+    } = section;
+
+    for (const text of content) {
+        await writeText(pdfPage, text, section.config)
+    }
 }
 
+async function writeText(pdfPage, text, sectionConfig) {
+    const {
+        text: contentText,
+        config: {
+            align = 'left',
+            bold = false,
+            italic = false,
+            color = 'black'
+        } = {}
+    } = text;
+    pdfPage.drawText(contentText, {
+        x: sectionConfig.marginLeft,
+        y: sectionConfig.marginTop,
+        size: sectionConfig.fontSize
+    })
+}
 
 async function buildPage(pdfDoc, page) {
 
     const {
-        config: {
-            fontSize,
-            marginTop,
-            marginBottom,
-            marginLeft,
-            marginRight
-        },
+        config: { fontSize, marginTop, marginBottom, marginLeft, marginRight },
         sections
     } = page;
 
     const pdfPage = pdfDoc.addPage(PageSizes.A4);
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+    pdfPage.setFont(helveticaFont)
 
-    //can we create these functions?
-    for (let idx = 0; idx < array.length; idx++) {
-        const element = array[idx];
-
-        
+    for (const section of sections) {
+        await drawSection(pdfDoc, section, page.config)
     }
-    await setFontSize(pdfPage, page.config.fontSize);
-    await setMargins(pdfPage, page.config)
-
 
 }
 
 module.exports = {
     buildPage,
 }
+
+/**
+   await setFontSize(pdfPage, page.config.fontSize);
+   
+    await setMargins(pdfPage, page.config) */
